b271d39c57db37b974e09c2d8c64f1b40bce2f64
Samuel Cooksey <slamcooksey@gmail.com>
2016-09-27 13:25:46 -0600

    change paths

--- scripts/angular/app.js
+++ scripts/angular/app.js
@@ -5,17 +5,17 @@ var url = 'https://operation-atlantis.herokuapp.com';
 app.config(function($routeProvider){
   $routeProvider
   .when('/', {
-    templateUrl: '/templates/home.html',
+    templateUrl: './templates/home.html',
     controller: 'homeController',
     controllerAs: 'HC'
   })
   .when('/oracle/:id', {
-    templateUrl:'/templates/oracleView.html',
+    templateUrl:'./templates/oracleView.html',
     controller: 'oracleController',
     controllerAs: 'OC'
   })
   .when('/runner/:id', {
-    templateUrl:'/templates/runnerView.html',
+    templateUrl:'./templates/runnerView.html',
     controller: 'runnerController',
     controllerAs: 'RC'
   })
