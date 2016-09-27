391d4476b91b4bdc5db4f78ded393af199d19b87
Samuel Cooksey <slamcooksey@gmail.com>
2016-09-27 13:45:12 -0600

    path change

--- scripts/angular/directives.js
+++ scripts/angular/directives.js
@@ -1,12 +1,12 @@
 'use strict';
 app.directive('headerDirective', [function(){
   return {
-    templateUrl: '/partials/header.html'
+    templateUrl: './partials/header.html'
   };
 }]);
 
 app.directive('footerDirective', [function(){
   return {
-    templateUrl: '/partials/footer.html'
+    templateUrl: './partials/footer.html'
   };
 }]);
